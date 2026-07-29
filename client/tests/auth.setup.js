03646cc0cd65a07ddfb8ce4dc0af21fc060b3b31
arunjo96 <arunkumarvadivel96@gmail.com>
2026-07-29 13:22:35 +0530

    Refactor authentication setup in Playwright tests to use placeholder selectors

--- client/tests/auth.setup.js
+++ client/tests/auth.setup.js
@@ -25,12 +25,38 @@
 
 import { test as setup, expect } from "@playwright/test";
 
+// setup("authenticate", async ({ page }) => {
+//   await page.goto("/login");
+
+//   await page.fill('input[name="email"]', "arunjo9601@gmail.com");
+
+//   await page.fill('input[name="password"]', "password");
+
+//   await page
+//     .getByRole("button", {
+//       name: /login/i,
+//     })
+//     .click();
+
+//   // wait for login complete
+//   await expect(page).not.toHaveURL(/login/);
+
+//   // verify authenticated UI
+//   await expect(page.getByTestId("profile-menu-button")).toBeVisible({
+//     timeout: 15000,
+//   });
+
+//   await page.context().storageState({
+//     path: "playwright/.auth/user.json",
+//   });
+// });
+
 setup("authenticate", async ({ page }) => {
   await page.goto("/login");
 
-  await page.fill('input[name="email"]', "arunjo9601@gmail.com");
+  await page.getByPlaceholder("example@gmail.com").fill("arunjo9601@gmail.com");
 
-  await page.fill('input[name="password"]', "password");
+  await page.getByPlaceholder("Enter password").fill("password");
 
   await page
     .getByRole("button", {
@@ -38,10 +64,8 @@ setup("authenticate", async ({ page }) => {
     })
     .click();
 
-  // wait for login complete
   await expect(page).not.toHaveURL(/login/);
 
-  // verify authenticated UI
   await expect(page.getByTestId("profile-menu-button")).toBeVisible({
     timeout: 15000,
   });
