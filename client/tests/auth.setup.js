8b99053ccb84e65541ccb28d90dde2bfe6dc7cb4
arunjo96 <arunkumarvadivel96@gmail.com>
2026-07-29 12:44:08 +0530

    Add Playwright configuration and update .gitignore for test artifacts

--- client/tests/auth.setup.js
+++ client/tests/auth.setup.js
@@ -1,4 +1,27 @@
+// import { test as setup, expect } from "@playwright/test";
 
+// setup("authenticate", async ({ page }) => {
+//   await page.goto("/login");
+
+//   await page.fill('input[name="email"]', "arunjo9601@gmail.com");
+
+//   await page.fill('input[name="password"]', "password");
+
+//   await page
+//     .getByRole("button", {
+//       name: /login/i,
+//     })
+//     .click();
+
+//   await expect(page).not.toHaveURL(/login/);
+
+//   // wait refresh token
+//   await page.waitForTimeout(3000);
+
+//   await page.context().storageState({
+//     path: "playwright/.auth/user.json",
+//   });
+// });
 
 import { test as setup, expect } from "@playwright/test";
 
@@ -15,10 +38,13 @@ setup("authenticate", async ({ page }) => {
     })
     .click();
 
+  // wait for login complete
   await expect(page).not.toHaveURL(/login/);
 
-  // wait refresh token
-  await page.waitForTimeout(3000);
+  // verify authenticated UI
+  await expect(page.getByTestId("profile-menu-button")).toBeVisible({
+    timeout: 15000,
+  });
 
   await page.context().storageState({
     path: "playwright/.auth/user.json",
